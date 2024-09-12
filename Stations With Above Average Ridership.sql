095114be4a03877d19a5846efc36af6075d1af92
Vincent Lee <128277296+LeeVincent3231@users.noreply.github.com>
2024-09-12 01:25:23 -0700

    Delete Stations With Above Average Ridership.sql

--- Stations With Above Average Ridership.sql
+++
@@ -1,7 +0,0 @@
--- identify stations with ridership exceeding the average daily trips across the system
-SELECT
-start_station_name,
-ROUND(AVG(tripduration),2) AS average_trip_length
-FROM `bigquery-public-data.new_york.citibike_trips`
-GROUP BY 1
-ORDER BY average_trip_length DESC;
